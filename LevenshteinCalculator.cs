67da221e9230cc48afe56de9745aeb2a9323ad7c
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-16 09:38:01 +0300

    Implement LevenshteinCalculator.cs, LongestCommonSubsequenceCalculator.cs

--- LevenshteinCalculator.cs
+++ LevenshteinCalculator.cs
@@ -14,11 +14,33 @@ namespace Antiplagiarism
     {
         public List<ComparisonResult> CompareDocumentsPairwise(List<DocumentTokens> documents)
         {
-            return new List<ComparisonResult> {
-                new ComparisonResult(
-                    documents[0], 
-                    documents[1], 
-                    TokenDistanceCalculator.GetTokenDistance(documents[0][0], documents[1][0]))};
+            var result = new List<ComparisonResult>();
+            for (int i = 0; i < documents.Count; i++)
+                for (int j = i + 1; j < documents.Count; j++)
+                    result.Add(CompareDocuments(documents[i], documents[j]));
+            return result;
+        }
+
+        ComparisonResult CompareDocuments(DocumentTokens first, DocumentTokens second)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+            double diff;
+            double[,] m = new double[first.Count + 1, second.Count + 1];
+
+            for (int i = 0; i <= first.Count; i++) { m[i, 0] = i; }
+            for (int j = 0; j <= second.Count; j++) { m[0, j] = j; }
+
+            for (int i = 1; i <= first.Count; i++)
+                for (int j = 1; j <= second.Count; j++)
+                {
+                    diff = (first[i - 1] == second[j - 1]) ? 0 : TokenDistanceCalculator.GetTokenDistance(first[i - 1], second[j - 1]);
+
+                    m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
+                                             m[i, j - 1] + 1),
+                                             m[i - 1, j - 1] + diff);
+                }
+            return new ComparisonResult(first, second, m[first.Count, second.Count]);
         }
     }
 }
--- LongestCommonSubsequenceCalculator.cs
+++ LongestCommonSubsequenceCalculator.cs
@@ -7,18 +7,36 @@ namespace Antiplagiarism
     {
         public static List<string> Calculate(List<string> first, List<string> second)
         {
-            var opt = CreateOptimizationTable(first, second);
-            return RestoreAnswer(opt, first, second);
+            int[,] opt = new int[first.Count + 1, second.Count + 1];
+            for (int i = 0; i < first.Count; i++)
+                opt[i, 0] = 0;
+            for (int j = 0; j < second.Count; j++)
+                opt[0, j] = 0;
+            for (int i = 1; i <= first.Count; i++)
+                for (int j = 1; j <= second.Count; j++)
+                {
+                    if (first[i - 1] == second[j - 1])
+                        opt[i, j] = opt[i - 1, j - 1] + 1;
+                    else
+                        opt[i, j] = Math.Max(opt[i, j - 1], opt[i - 1, j]);
+                }
+            return BacktrackOpt(opt, first, second, first.Count, second.Count);
         }
 
-        private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
+        private static List<string> BacktrackOpt(int[,] opt, List<string> first, List<string> second, int x, int y)
         {
-            throw new NotImplementedException();
-        }
-
-        private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
-        {
-            throw new NotImplementedException();
+            if (x == 0 | y == 0)
+                return new List<string>();
+            if (first[x - 1] == second[y - 1])
+            {
+                // Здесь используется рекурсия, которую не тривиально заменить на цикл
+                var result = BacktrackOpt(opt, first, second, x - 1, y - 1);
+                result.Add(first[x - 1]);
+                return result;
+            }
+            if (opt[x, y - 1] > opt[x - 1, y])
+                return BacktrackOpt(opt, first, second, x, y - 1);
+            return BacktrackOpt(opt, first, second, x - 1, y);
         }
     }
 }
